174c3eea58f0ee4646a037bf187b2751b7a4a24e
Vision Rijal <82150577+visionrijal@users.noreply.github.com>
2026-04-25 18:44:09 +0545

    Fix installation command for print_server.py

--- setup.sh
+++ setup.sh
@@ -158,7 +158,6 @@ cd "$INSTALL_DIR"
 # --- Install the print server script ------------------------------------------
 
 log "Installing print_server.py..."
-cp "$(dirname "$0")/print_server.py" "$INSTALL_DIR/print_server.py"
 chmod +x "$INSTALL_DIR/print_server.py"
 
 # --- Configure CUPS -----------------------------------------------------------
